2221e2b9eeed9c7e84a4d5faf2561a3fb5fe04e8
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-02-01 19:10:53 +0000

    feat: absorb heroicons into /priv and use  and  in both  and

--- flake.nix
+++ flake.nix
@@ -4,8 +4,6 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
-    heroicons.url = "github:tailwindlabs/heroicons/v2.1.1";
-    heroicons.flake = false;
     systems.url = "github:nix-systems/default";
   };
   outputs =
@@ -28,6 +26,22 @@
           version = builtins.head (
             builtins.match ".*version:[[:space:]]*\"([0-9]+\\.[0-9]+\\.[0-9]+)\".*" mixExs
           );
+          translatedPlatform =
+            {
+              aarch64-darwin = "macos-arm64";
+              aarch64-linux = "linux-arm64";
+              armv7l-linux = "linux-armv7";
+              x86_64-darwin = "macos-x64";
+              x86_64-linux = "linux-x64";
+            }
+            .${system};
+          preInstall = ''
+            ln -s ${pkgs.tailwindcss}/bin/tailwindcss _build/tailwind-${translatedPlatform}
+            ln -s ${pkgs.esbuild}/bin/esbuild _build/esbuild-${translatedPlatform}
+
+            ${pkgs.elixir}/bin/mix assets.deploy
+            ${pkgs.elixir}/bin/mix phx.gen.release
+          '';
         in
         {
           devShells.default = pkgs.mkShell {
@@ -39,41 +53,29 @@
           };
 
           packages.fod =
-            let
-              translatedPlatform =
-                {
-                  aarch64-darwin = "macos-arm64";
-                  aarch64-linux = "linux-arm64";
-                  armv7l-linux = "linux-armv7";
-                  x86_64-darwin = "macos-x64";
-                  x86_64-linux = "linux-x64";
-                }
-                .${system};
-            in
-            with pkgs;
             with pkgs.beamPackages;
             mixRelease {
-              inherit pname version src;
+              inherit
+                pname
+                version
+                src
+                preInstall
+                ;
               mixFodDeps = fetchMixDeps {
                 inherit version src pname;
                 sha256 = "sha256-bEaxsw3OdRoiGoi1Vv2k0QxNT/PdGDGsOf5UDho3L1o=";
-                buildInputs = [ ];
-                propagatedBuildInputs = [ ];
               };
-              preInstall = ''
-                ln -s ${tailwindcss}/bin/tailwindcss _build/tailwind-${translatedPlatform}
-                ln -s ${esbuild}/bin/esbuild _build/esbuild-${translatedPlatform}
-
-                ${elixir}/bin/mix assets.deploy
-                ${elixir}/bin/mix phx.gen.release
-              '';
             };
 
           packages.deps_nix =
-            with pkgs;
             with pkgs.beamPackages;
-            mixRelease {
-              inherit pname version src;
+            beamPackages.mixRelease {
+              inherit
+                pname
+                version
+                src
+                preInstall
+                ;
               mixNixDeps = import ./hello/deps.nix { inherit lib beamPackages pkgs; };
             };
         };
